dfa722543f8d80f2ad9cbec41f88fef3f9ef9581
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-12-04 16:23:55 +0300

    refactor: deleted etra ApiProperty

--- src/chat/dto/create-chat-reponse.dto.ts
+++ src/chat/dto/create-chat-reponse.dto.ts
@@ -2,6 +2,5 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateChatResponseDto {
   @ApiProperty({ example: 1, description: 'Unique ID of the message' })
-  @ApiProperty({ example: 1, description: 'Unique ID of the message' })
   id: number;
 }
